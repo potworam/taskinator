3a122a81c01648c30f94ccda520bc605e9d35afb
Potwora <matthewpotwora@yahoo.com>
2022-05-22 22:18:19 -0700

    add sub text

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,21 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
-
   event.preventDefault();
-
-    var listItemEl = document.createElement("li");
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+  var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className ="task-info";
+    
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
-    console.log(event);
+   
   };
 
   formEl.addEventListener("submit", createTaskHandler);
